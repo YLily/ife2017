1a2d96cd3556cc968c2703d789b3cd0c8039d17d
YLily <1162930942@qq.com>
2017-03-08 12:34:30 +0800

    add datavisible

---
+++ echarts/build/csvtojson.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const fs = require("fs")
+
+const csv=require('csvtojson')
+
+var rootPath = path.resolve(__dirname, '..');
+const csvFilePath= path.join(rootPath, 'src/sp500hst.txt');
+
+var arr = [];
+
+csv()
+.fromFile(csvFilePath)
+.on('json',(jsonObj, index)=>{
+	arr[index] = jsonObj;
+})
+.on('done',(error)=>{
+    fs.writeFile(path.join(rootPath,'src/data.json'), JSON.stringify(arr));
+})
